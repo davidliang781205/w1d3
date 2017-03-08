c079ea82f9a01e9fb63840890dba45ff03ee1a4d
David <davidliang781205@gmail.com>
2017-03-08 01:45:32 +0000

    Completed CharacterCounting.js

---
+++ CharacterCounting/CharacterCounting.js
@@ -0,0 +1,21 @@
+function countLetters(str){
+  str = str.split(" ");
+  str = str.join("");
+  var o = {};
+
+  for (var i = 0; i < str.length; i++) {
+
+
+    if (typeof o[str[i]] === 'undefined'){
+      o[str[i]] = 1
+    } else {
+      o[str[i]] += 1;
+    }
+  }
+  return o;
+}
+
+
+
+
+console.log(countLetters("lighthouse in the house"));
